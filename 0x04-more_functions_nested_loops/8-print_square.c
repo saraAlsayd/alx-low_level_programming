12ad1d52324a9a4682bba5c2a211cf5e0f6f471b
saraAlsayd <roraya2442@gmail.com>
2023-04-27 13:48:54 -0700

    sdf

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,13 +6,22 @@
  */
 void print_square(int size)
 {
-int row, column;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
 
-for (row = 1; row <= size; row++)
-{
-	for (column = 1; column <= size; column)
-		_putchar('#');
-	_putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
-}
+
 
