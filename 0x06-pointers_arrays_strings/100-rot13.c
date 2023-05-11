de55c2397f16270e5abb7cd1aa50556d82ae4c0f
saraAlsayd <roraya2442@gmail.com>
2023-05-11 06:37:10 -0700

    fds

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *
+ *function rot13 -encoder rot13
+ *
+ *@c:pointer to program
+ *
+ * return :*c
+ */
+char *rot13(char *c)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == data1[j])
+			{
+				c[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
+
