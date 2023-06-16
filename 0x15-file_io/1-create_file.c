a7ca8c1d1811c23cad2d8fc0afc387d11bf07499
saraAlsayd <roraya2442@gmail.com>
2023-06-16 03:57:53 -0700

    jhdsa

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file to Creates file.
+ * @filename: A pointer to name file to create it.
+ * @text_content: A pointer of string to write in the file.
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: If the function fails -1.
+ * Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
