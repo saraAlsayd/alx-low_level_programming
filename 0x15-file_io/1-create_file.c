1cf7b2b3d0192853d9af78ea30321e257e57089d
saraAlsayd <roraya2442@gmail.com>
2023-06-16 04:00:40 -0700

    error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
-	w = write(fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 066666666 = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
