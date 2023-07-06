1ef1cab34e94b4faa470dc32d2a09ccde379690d
saraAlsayd <roraya2442@gmail.com>
2023-07-06 07:23:48 -0700

    huoi

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	int j;
 	unsigned int decval = 0;
 
-	if (!b)
-		return (0);
-
 	for (j = 0; b[j]; j++)
 	{
 		if (b[j] < '0' || b[j] > '1')
 			return (0);
 		decval = 2 * decval + (b[j] - '0');
 	}
+	
+		if (!b)
+		return (0);
 
 	return (decval);
 }
