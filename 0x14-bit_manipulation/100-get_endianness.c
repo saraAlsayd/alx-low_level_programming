48c00f4cf808daf67aa893227e479cf0bf8ff784
saraAlsayd <roraya2442@gmail.com>
2023-07-07 10:22:06 -0700

    34567

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,15 +5,11 @@
  *
  * Return: 0 if big endian, 1 if little endian
  */
-
 int get_endianness(void)
 {
-	unsigned int i;
-	char *c;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-	i = 1;
-	c = (char *) & i;
-
-	return ((int)*c);
+	return (*c);
 }
 
