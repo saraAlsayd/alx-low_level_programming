b62d7a3f11c39dc25b037db94d946e3dc0cbe9cb
saraAlsayd <roraya2442@gmail.com>
2023-07-07 12:14:25 -0700

    final

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,11 +11,11 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		putchar((n & 1) + '0');
 	}
 	else
 	{
-		_putchar('0');
+		putchar('0');
 	}
 }
 
