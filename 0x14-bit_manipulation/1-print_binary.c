8044c73602b7855b01063dd8355f2b1a0e9b1355
saraAlsayd <roraya2442@gmail.com>
2023-07-07 10:05:48 -0700

    fghor

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n:the unsigned long int.
+ * print_binary -it's prints the binary representation
+ * of a the number.
+ * @n: unsigned long int.
  *
- * Return: no thing.
+ * Return: no thing to return.
  */
 void print_binary(unsigned long int n)
 {
-int i;
-for (i = 31; i >= 0; i--)
-(n >> i) & 1;
-if (n == '0')
-putchar('0');
-else if (n == '1')
-putchar ('1');
-else
-putchar('0');
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
 
